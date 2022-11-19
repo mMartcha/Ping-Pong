199e270568d50cf594b1c8595f3d74b9b9d7356c
Marcello Stefenon Filho e Guilherme Almeida Franciosi <Stefenonmarcello@gmail.com>
2022-11-19 18:54:13 -0300

    commit

--- pingPong.py
+++ pingPong.py
@@ -11,7 +11,7 @@ raqueteUm.color("white")
 raqueteUm.shape("square")
 raqueteUm.shapesize(largura=5, altura=1)  # 20*5 height
 raqueteUm.penup()
-raqueteUm.goto(-350, 0)    # (0, 0) is in middle
+raqueteUm.goto(-350, 0)    
 larguraUm = 5
 #   2nd Paddle
 raqueteDois = turtle.Turtle()
@@ -20,18 +20,17 @@ raqueteDois.color("white")
 raqueteDois.shape("square")
 raqueteDois.shapesize(largura=5, altura=1)
 raqueteDois.penup()
-raqueteDois.goto(350, 0)    # (0, 0) is in middle
+raqueteDois.goto(350, 0)    
 larguraDois = 5
-#   bola
 bola = turtle.Turtle()
-bola.speed(0)     # speed of animation, '0' for MAX
+bola.speed(0)     
 bola.color("white")
 bola.shape("circle")
 bola.penup()
-bola.goto(0, 0)    # (0, 0) is in middle
-bola.dx = 0.2     # bola moves by 2 pixels
+bola.goto(0, 0)    
+bola.dx = 0.2     
 bola.dy = -0.2
-# for scoring
+
 pontuacaoUm = 0
 pontuacaoDois = 0
 escritaDePontuacao = turtle.Turtle()
@@ -40,46 +39,43 @@ escritaDePontuacao.color("white")
 escritaDePontuacao.penup()
 escritaDePontuacao.hideturtle()
 escritaDePontuacao.goto(0, 260)
-escritaDePontuacao.write("Player One: 0        Player Two: 0", align="center", font=("Courier", 24, "normal"))
-# movement of paddle
+escritaDePontuacao.write("Jogador Um: 0        Jogador Dois: 0", align="Centro", font=("Courier", 24, "normal"))
+
 def raqueteUm_up():
-    y = raqueteUm.ycor()   # coordinates
+    y = raqueteUm.ycor()   
     y += 50
     raqueteUm.sety(y)
 def raqueteUm_down():
-    y = raqueteUm.ycor()   # coordinates
+    y = raqueteUm.ycor()   
     y -= 50
     raqueteUm.sety(y)
 def raqueteDois_up():
-    y = raqueteDois.ycor()   # coordinates
+    y = raqueteDois.ycor()  
     y += 50
     raqueteDois.sety(y)
 def raqueteDois_down():
-    y = raqueteDois.ycor()   # coordinates
+    y = raqueteDois.ycor()   
     y -= 50
     raqueteDois.sety(y)
-# Keyboard Events
+
 janela.listen()
-# Left one
 janela.onkeypress(raqueteUm_up, 'w')
 janela.onkeypress(raqueteUm_down, 's')
-# right one
 janela.onkeypress(raqueteDois_up, 'Up')
 janela.onkeypress(raqueteDois_down, 'Down')
-# main loop for the game to run
+
 while True:
     janela.update()
-    # bola Movement
     bola.setx(bola.xcor() + bola.dx)
     bola.sety(bola.ycor() + bola.dy)
-    # bola's Border checking
+
     if bola.ycor() > 290:
         bola.sety(290)
-        bola.dy *= -1   # reversing direction
+        bola.dy *= -1   
     if bola.ycor() < -290:
         bola.sety(-290)
-        bola.dy *= -1   # reversing direction
-    if bola.xcor() > 390:   # past the paddle
+        bola.dy *= -1   
+    if bola.xcor() > 390:   
         bola.goto(0, 0)
         bola.dx = -0.2
         if bola.dy > 0 : 
@@ -96,7 +92,7 @@ while True:
             larguraDois += 1
             raqueteUm.shapesize(largura=larguraUm, altura=1)
             raqueteDois.shapesize(largura=larguraDois, altura=1)
-    if bola.xcor() < -390:   # past the paddle
+    if bola.xcor() < -390:   
         bola.goto(0, 0)
         bola.dx = 0.2
         if bola.dy > 0 : 
@@ -112,7 +108,6 @@ while True:
             larguraUm += 1
             raqueteDois.shapesize(largura=larguraDois, altura=1)
             raqueteUm.shapesize(largura=larguraUm, altura=1)
-    # Collisions b/w bola & paddle
     if (340 < bola.xcor() < 350) and (raqueteDois.ycor() + 40 > bola.ycor() > raqueteDois.ycor() - 40):
         bola.setx(340)
         bola.dx *= -1.05
