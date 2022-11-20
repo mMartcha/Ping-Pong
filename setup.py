edae548e0ea54d65afa6328ba9e87ea958aa68c3
Marcello Stefenon Filho e Guilherme Almeida Franciosi <Stefenonmarcello@gmail.com>
2022-11-20 20:08:35 -0300

    commit

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name = "Ping Pong" ,
         "build_exe":{"packages":["pygame"],
         "include_files":["assets"]
         }},
-    description = "Esse foi um projeto criado pelos alunos Henrique Tres Terra e Vinicius Artuso da instituição ATITUS, Passo Fundo" ,
+    description = "Esse foi um projeto criado pelos alunos Marcello Stefenon Filho e Guilherme Almeida Franciosi da instituição ATITUS, Passo Fundo" ,
     executables = [Executable("pingPong.py")])
