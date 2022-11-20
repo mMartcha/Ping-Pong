cd2a9c5837caf9ae7fe52e7493a1ba9f8b812343
Marcello Stefenon Filho e Guilherme Almeida Franciosi <Stefenonmarcello@gmail.com>
2022-11-20 20:22:37 -0300

    commit

--- script.py
+++ script.py
@@ -2,9 +2,8 @@ from cx_Freeze import setup, Executable
 
 setup(name = "Ping Pong" ,
     version = "1.0" ,
-    options={
-        "build_exe":{"packages":["pygame"],
-        "include_files":["assets"]
-        }},
+    options={"build_exe":{"packages":["pygame"],"include_files":["assets"]}},
     description = "Esse foi um projeto criado pelos alunos Marcello Stefenon Filho e Guilherme Almeida Franciosi da instituição ATITUS, Passo Fundo" ,
     executables = [Executable("pingPong.py")])
+
+
